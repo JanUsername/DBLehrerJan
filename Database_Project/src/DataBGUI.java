91bc8f724e1df54bdf52661b3578fc5b7daefbfb
root <root@ubuntu>
2014-03-22 16:13:23 +0100

    database gui, jeah

--- Database_Project/src/DataBGUI.java
+++ Database_Project/src/DataBGUI.java
@@ -5,13 +5,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 
 public class DataBGUI extends JFrame {
-	/**
-	 * @wbp.nonvisual location=48,27
-	 */
-	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 
 	/**
 	 * Launch the application.
@@ -33,11 +32,30 @@ public class DataBGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public DataBGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 567, 416);
+		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		getContentPane().add(tabbedPane);
 		
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("New tab", null, panel, null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		panel.setLayout(null);
+		
+		JLabel lblHalloJanSchler = new JLabel("Order");
+		lblHalloJanSchler.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblHalloJanSchler.setBounds(12, 12, 151, 15);
+		panel.add(lblHalloJanSchler);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(12, 39, 70, 15);
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
 	}
-
 }
